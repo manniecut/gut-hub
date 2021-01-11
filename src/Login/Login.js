0c01ad9025c4d2fe9c9751cd5af71c7a5b022505
Mannie Cutean <68138888+manniecut@users.noreply.github.com>
2021-01-11 14:40:03 -0500

    Update Login.js

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -104,7 +104,7 @@ class Login extends Component {
                             Login
                 </button>
                     </div>
-                    <p>This is a test version, you can sign in with: "Guest", "guest".</p>
+                    <p>You can also sign in with: "Guest", "guest".</p>
                     <p>Or <Link to='/createaccount'>Create an Account</Link></p>
                 </form>
             </>
